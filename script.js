2f598dbb534dbbb35548b9dc22a0632bc798bf73
areo-hotah <hellionwins@gmail.com>
2022-08-13 15:18:20 +0100

    Updated the script so the display updates when the user presses a button.
    When that button is pressed it's value is displayed on the screen.
    
    I still need to place the value in a data structure though.

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// selecting the display
+let display = document.querySelector(".display");
+
+// selecting all the buttons with num class
+let numbers = document.querySelectorAll(".num");
+
+let operate = document.querySelectorAll(".operate");
+
+// function that updates the display
+/* let updateDisplay = ()=>{
+ *   //display.textContent = this.textContent;
+ *   console.log("this works")
+}*/
+
+
+// attaching event listeners to buttons
+numbers.forEach(
+  (button) => button.addEventListener('click', (e)=> display.textContent += e.target.textContent));
+
+operate.forEach(
+  (button) => button.addEventListener('click', (e)=> display.textContent += e.target.textContent));
+
+
+
+
 let operators = {
   add(a,b) {
     return a + b;
@@ -16,6 +41,6 @@ let operators = {
   },
 
   operate(operator,a,b) {
-	return operator(a,b);
+  return operator(a,b);
   }
 }
